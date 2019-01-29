12320e084b2abd69ed11692775433166ff84a2b8
Olivia Wan <wan.o@husky.neu.edu>
2019-01-29 14:59:46 -0500

    block flipping more than two cards, or unflipping cards

--- assets/js/memory-game.jsx
+++ assets/js/memory-game.jsx
@@ -12,6 +12,8 @@ class Memory extends React.Component {
         this.state = {
             grid: this.buildGrid(),
             numFlipped: 0,
+            viewed: {},
+            currentCards: []
         };
     }
 
@@ -20,7 +22,7 @@ class Memory extends React.Component {
         let values = [];
         for (let i = 0; i < 16; i++) {
             let val = ((i % 8) + 10).toString(36);
-            values.push(val);
+            values.push(val.toUpperCase());
         }
         return _.shuffle(values);
     }
@@ -47,22 +49,61 @@ class Memory extends React.Component {
     flipCard(index) {
         let row = index[0];
         let col = index[1];
-
         let card = this.state.grid[row][col];
-        let newGrid = this.state.grid.slice();
-        newGrid[row][col] = {
-            value: card.value,
-            flipped: !card.flipped
-        };
 
-        this.setState({grid: newGrid});
+        if (this.state.numFlipped < 2 && !card.flipped) {
+            let newGrid = this.state.grid.slice();
+            newGrid[row][col] = {
+                value: card.value,
+                flipped: true
+            };
+
+            let currentCards = this.state.currentCards.slice();
+            currentCards.push(card);
+
+            let numFlipped = this.state.numFlipped;
+            numFlipped += 1;
+
+            this.setState({
+                grid: newGrid,
+                numFlipped: numFlipped,
+                currentCards: currentCards
+            });
+
+            if (numFlipped === 2) {
+                setTimeout(() => {
+                    this.resetFlips();
+                }, 1000);
+            }
+        }
+    }
+
+    //hide all cards
+    resetFlips() {
+        let newGrid = this.state.grid.slice();
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                let card = newGrid[i][j];
+                if (card.flipped) {
+                    newGrid[i][j] = {
+                        value: card.value,
+                        flipped: false
+                    }
+                }
+            }
+        }
+        this.setState({grid: newGrid, numFlipped: 0, currentCards: []});
     }
 
     render() {
         let rows = _.map(this.state.grid, (row, ii) => {
             return <RowItem row={row} index={ii} key={ii} flipCard={this.flipCard.bind(this)}/>;
         });
+        let currentCards = _.map(this.state.currentCards, (card, ii) => {
+            return <span key={ii}>{card.value}</span>;
+        });
         return <div id="grid">
+            Current cards: {currentCards}
             {rows}
         </div>;
     }
